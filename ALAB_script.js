97aacfa12d48598c4579419a31f87f66bfe04265
userlaaame <daddystarbuqs@disroot.org>
2026-05-09 03:47:32 -0400

    updated JS part 1

--- ALAB_script.js
+++ ALAB_script.js
@@ -12,41 +12,98 @@
 // const area = PI * radius * radius;
 // Each plant requires a minimum space of 0.8 square meters.
 
+console.log("--- Part 1: Growing Pains ---");
+
 const PI = 3.1415;
 const radius = 5;
 //minimum space of a single plant
-const plant = 0.8;
-const area = PI * radius * radius
+const plantSpaceReq = 0.8;
+//Area space for plants
+const area = PI * radius * radius; //78.5375
 //starting number of plants
 let plantcount = 20;
-const capacity = area / plant
+const capacity = area / plantSpaceReq;
 
-console.log('Area: ', area)
-
-console.log(plant + radius);
-
-if (true) {
-    console.log('Prune')
-} else if (true) {
-    console.log('Monitor')
-} else {
-    console.log('Planted')
-}
 // The area is starting with 20 plants.
 // The plants double in number every week.
+//Let's make a variable for weeks
+let week1 = plantcount * 2;       // 40 plants after week 1
+let week2 = plantcount * 4;       // 80 plants after week 2
+let week3 = plantcount * 8;       // 160 plants after week 3
+//Can I divide plantspace into area by the number of weeks?
+//console.log(area / week1count);  
+
+//I need to multiply plantcount by plantSpaceReq then divide by area
+console.log("========Weekly Results======");
+// console.log((week1 * plantSpaceReq) / area); //.40744867101703003 but what does that mean??
+//ahhhh it's about 40.7% Percent!....good to know. Now we need to do this
+//for the other weeks.
+let week1count = (week1 * plantSpaceReq) / area;
+let week2count = (week2 * plantSpaceReq) / area;
+let week3count = (week3 * plantSpaceReq) / area;
 // Using this information, your objectives are to:
 // Predict the plant growth after a specific number of weeks.
-// Implement control flow to make decisions on whether the plants should be:
-// Pruned, to stop them from exceeding the capacity of the garden.
-
-// This condition should be met if the plant count after the given number of weeks is greater 
-// than 80% of the maximum capacity of the garden.
-
+// Implement control flow to make decisions on whether the plants 
+// should be: Pruned, to stop them from exceeding the capacity 
+// of the garden.
+// This condition should be met if the plant count after the given
+//  number of weeks is greater than 80% of the maximum capacity
+//  of the garden.
 // Monitored, if they are growing at an acceptable rate.
-// This condition should be met if the plant count is between 50% and 80% of the 
-// maximum capacity of the garden after the given number of weeks.
-
+// This condition should be met if the plant count is between 
+// 50% and 80% of the maximum capacity of the garden after the
+//  given number of weeks.
 // Planted, if there is room to plant more plants.
-// This condition should be met if the plant count after the given number of
-//  weeks is less than 50% of the maximum capacity of the garden.
-// Within your submission, include the results for 1, 2, and 3 weeks of growth as inputs.//
+// This condition should be met if the plant count after the 
+// given number of weeks is less than 50% of the maximum capacity
+// of the garden.Within your submission, include the results for
+// 1, 2, and 3 weeks of growth as inputs.
+//here we go...
+// if (week1count >= .80 || week2count >= .80 || week3count >= .80); {
+//     console.log("Prune Garden")
+// } if else (week1count >= .50 || week2count >= .50 || week3count >= .50); {
+//     console.log("Monitor Garden");
+// } if else (week1count <= .50 || week2count <= .50 || week3count <= .50); {
+//     console.log("Planted");
+// }
+//didn't work....we'll try making all weeks at once
+if (week1count >= 0.80) {
+    console.log("Week 1: Prune Garden");
+} else if (week1count >= 0.50) {
+    console.log("Week 1: Monitor Garden");
+} else {
+    console.log("Week 1: Plant More");
+}
+
+if (week2count >= 0.80) {
+    console.log("Week 2: Prune Garden");
+} else if (week2count >= 0.50) {
+    console.log("Week 2: Monitor Garden");
+} else {
+    console.log("Week 2: Plant More");
+}
+
+if (week3count >= 0.80) {
+    console.log("Week 3: Prune Garden");
+} else if (week2count >= 0.50) {
+    console.log("Week 3: Monitor Garden");
+} else {
+    console.log("Week 3: Plant More");
+}
+
+console.log("==========Weekly Metrics===========");
+console.log(week1,"plants");
+console.log(week2,"plants");
+console.log(week3,"plants");
+
+// console.log('Area: ', area);
+// console.log(plantSpaceReq + radius);
+// if (true) {
+//     console.log('Prune')
+// } else if (true) {
+//     console.log('Monitor')
+// } else {
+//     console.log('Planted')
+// } 
+
+console.log("================Part 2: Thinking Bigger======================")
